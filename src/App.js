fee2f5cdd8655255542026f89060ebec97d7b91d
nog <nogueira@pm.ba.gov.br>
2020-05-01 20:52:00 -0300

    Exibindo lista json na tela usando jquery

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React, {Component} from 'react';
 import './css/pure-min.css';
 import './css/side-menu.css';
+import $ from 'jquery';
 
 class App extends Component{
 
 constructor(){
   super();
-  this.state = {lista : [{
-    nome:'Nogueira',
-    email:'nog@caelum.com.br',
-    senha:'123456'
-  }]};
+  this.state = {lista : []};
+}
+
+componentWillMount(){
+  $.ajax({
+      url:"http://cdc-react.herokuapp.com/api/autores",
+      dataType: 'json',
+      success:function(resposta){
+        this.setState({lista:resposta}); 
+      }.bind(this)
+    }
+   );
 }
 
 render() {
